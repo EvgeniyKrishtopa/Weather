a9653de55801d9e04bac986b104e7d71e794b3cd
EvgeniyKrishtopa <evgeniykrishtopa@gmail.com>
2020-02-28 14:00:05 +0200

    Updates

---
+++ precache-manifest.4f0cb9b410d82b01538fa43c6ab86106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07f023bc27cf33011c5ea1d9d312e86",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "d03493bd7090c0384e56",
+    "url": "/Weather/static/css/main.44ae90a9.chunk.css"
+  },
+  {
+    "revision": "1694ac63ce246fe21be6",
+    "url": "/Weather/static/js/2.a64ce9e6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Weather/static/js/2.a64ce9e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03493bd7090c0384e56",
+    "url": "/Weather/static/js/main.6524bf68.chunk.js"
+  },
+  {
+    "revision": "cc0e8d60c36f34722811",
+    "url": "/Weather/static/js/runtime-main.04b6cc88.js"
+  },
+  {
+    "revision": "a56913c88ef7f5a2770ae5e6c41bebdd",
+    "url": "/Weather/static/media/weather-bg.a56913c8.jpg"
+  }
+]);
